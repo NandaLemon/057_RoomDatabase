113505014cf59afdf684a950d3f815b08923caec
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 17:38:56 +0700

    create String resource id yang berisi judul akan ditampilkan dilayar halaman.

--- app/src/main/java/com/example/data_siswa/Navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/data_siswa/Navigasi/DestinasiNavigasi.kt
@@ -5,5 +5,8 @@ interface DestinasiNavigasi {
      * Nama unik untuk menentukan jalur untuk composable
      */
     val route: String
-
+    /**
+     * String resource id yang berisi judul akan ditampilkan dilayar halaman.
+     */
+    val titleRes: Int
 }
