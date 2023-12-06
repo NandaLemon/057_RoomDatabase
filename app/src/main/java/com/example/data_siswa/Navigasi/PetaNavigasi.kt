13895bc33d99a989d93637f8d56d06abd9717630
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 18:31:25 +0700

    create Host Navigasi

--- app/src/main/java/com/example/data_siswa/Navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/data_siswa/Navigasi/PetaNavigasi.kt
@@ -1,9 +1,29 @@
 package com.example.data_siswa.Navigasi
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController()) {
     HostNavigasi(navController = navController)
+}
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route,modifier = Modifier)
+    {
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
+    }
+}
