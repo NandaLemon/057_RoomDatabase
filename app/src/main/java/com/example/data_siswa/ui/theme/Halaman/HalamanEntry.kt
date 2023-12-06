3cd74123df0c1683f8b1dfc74c917f9c1d456876
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 19:10:06 +0700

    create inner pading

--- app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,19 +1,32 @@
 package com.example.data_siswa.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.data_siswa.Navigasi.DestinasiNavigasi
 import com.example.data_siswa.Navigasi.SiswaTopAppBar
 import com.example.data_siswa.R
+import com.example.data_siswa.model.DetailSiswa
 import com.example.data_siswa.model.EntryViewMode
 import com.example.data_siswa.model.PenyediaViewModel
+import com.example.data_siswa.model.UIStateSiswa
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -36,4 +49,20 @@ fun EntrySiswaScreen(
                 canNavigateBack = true,
                 scrollBehavior = scrollBehavior
             )
+}) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
