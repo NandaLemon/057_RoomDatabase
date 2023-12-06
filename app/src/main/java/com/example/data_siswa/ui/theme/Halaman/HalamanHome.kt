59c75e0582409b6a89755eaf34395849767f17c4
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 19:41:39 +0700

    create HomeScreen

--- app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,9 +1,23 @@
 package com.example.data_siswa.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.data_siswa.Navigasi.DestinasiNavigasi
 import com.example.data_siswa.R
+import com.example.data_siswa.model.HomeViewModel
+import com.example.data_siswa.model.PenyediaViewModel
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate:(Int) -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){}
