6ccb13f4bff9f784d84da5bbd24fa77f35a06d45
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 19:53:01 +0700

    add HomeScreen

--- app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,10 +1,24 @@
 package com.example.data_siswa.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.data_siswa.Navigasi.DestinasiNavigasi
+import com.example.data_siswa.Navigasi.SiswaTopAppBar
 import com.example.data_siswa.R
 import com.example.data_siswa.model.HomeViewModel
 import com.example.data_siswa.model.PenyediaViewModel
@@ -20,4 +34,29 @@ fun HomeScreen(
     navigateToItemUpdate:(Int) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        },
+    ){}
+}
