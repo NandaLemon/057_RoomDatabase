16f183da3e04cc0f40b598ec580c7d16d64c30ef
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 19:38:08 +0700

    create object DestinasiHome

--- app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.data_siswa.ui.theme.Halaman
 
+import com.example.data_siswa.Navigasi.DestinasiNavigasi
+import com.example.data_siswa.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
