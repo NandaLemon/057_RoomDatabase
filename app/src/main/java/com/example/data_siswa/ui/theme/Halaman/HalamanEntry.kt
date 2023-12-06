051507077675ea27666f3ced2c6cbc3714044e78
NandaLemon <rizkynanda010703@gmail.com>
2023-12-06 18:14:03 +0700

    create object destinasiEntry

---
+++ app/src/main/java/com/example/data_siswa/ui/theme/Halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.data_siswa.ui.theme.Halaman
+
+import com.example.data_siswa.Navigasi.DestinasiNavigasi
+import com.example.data_siswa.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
